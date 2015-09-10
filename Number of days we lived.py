f25c3aa97c3244943caf61d39d9e4828fe2bdf71
Zarcaxm <andrebloggergaming@gmail.com>
2015-09-11 00:22:14 +0100

    A small python project than tells u how many days u lived

---
+++ Number of days we lived.py
@@ -0,0 +1,10 @@
+__author__ = 'ZARCASM'
+import datetime
+
+
+userinfo=input("When did u born?(dd/mm/yyyy)")
+#convert userinfo
+bday = datetime.datetime.strptime(userinfo,"%d/%m/%Y").date()
+today = datetime.date.today()
+days = today - bday
+print(days)
